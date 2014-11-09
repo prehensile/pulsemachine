1ce3298a445c66eb1425417d0eafeaea89723c67
Henry Cooke <me@prehensile.co.uk>
2014-11-09 22:39:26 +0000

    improved dummy bpm stuffs, added pan

--- static/app.js
+++ static/app.js
@@ -17,17 +17,20 @@ $.extend( PulseListener.prototype, {
   sprite: null,
   isBeat: false,
   isHigh: false,
-  isDummy: false,
+  pan: 0,
   
   init: function( threshold, dummyBPM ){
     this.beatThreshold = threshold;
     this.isBeat = false;
     this.isHigh = false;
-    this.isDummy = dummyBPM > 0;
-    if( this.isDummy ) {
-      this.bpm = dummyBPM;
-      this.beatInterval = (dummyBPM/60) * 1000;
+    if( dummyBPM > 0 ) {
+      this.setBpm( dummyBPM );
     }
+  },
+
+  setBpm: function( bpmIn ){
+    this.bpm = bpmIn;
+    this.beatInterval = (bpmIn/60) * 1000;
   },
 
   logValue : function( valueIn ){
@@ -71,18 +74,18 @@ $.extend( PulseListener.prototype, {
     var l = arrIn.length;
     for (var i = l-1; i >= 0; i--) {
       total += arrIn[i];
-    };
+    }
     return total / l;
   },
 
   smoothedValue: function( newValue, oldValue, smoothing ){
     var val = (oldValue*this.smoothing) + (newValue*(1.0-this.smoothing));
-    if( isNaN(val) ) val = newValue;  
+    if( isNaN(val) ) val = newValue;
     return val;
   },
 
   normalisedValue: function(){
-    if( this.lastValue != 0 )
+    if( this.lastValue !== 0 )
       return (this.lastValue / this.maxValue);
     return 0;
   },
@@ -123,7 +126,7 @@ initWidgets = function(){
   $( "#sldXfade" ).slider();
   $( "#bankA" ).selectmenu();
   $( "#bankB" ).selectmenu();
-}
+};
 
 initSound = function(){
   $.getJSON( "/static/soundbanks.json", function( json ){
@@ -136,7 +139,7 @@ initSound = function(){
       }
     };
   });
-}
+};
 
 easeInOutExpo = function (t, b, c, d) {
     if (t==0) return b;
@@ -151,24 +154,27 @@ easeInOutQuad = function (t, b, c, d) {
   },
 
 playSoundForListenerIndex = function( index ){
-  console.log( 'playSoundForListenerIndex', index );
+  // console.log( 'playSoundForListenerIndex', index );
   index++;
   var idSoundA = $( "#bankA" ).val() + "_" + index;
   var idSoundB = $( "#bankB" ).val() + "_" + index;
   var xfade = $( "#sldXfade" ).slider( "value" ) / 100.0;
-  console.log(  idSoundA, idSoundB, xfade );
+  var pan = -2 * ((50.0 - ($( "#sldPan_" + index ).slider( "value" )) ) / 100.0);
+  // console.log( "pan", pan );
   if( xfade < 1.0 ) {
     var ins = createjs.Sound.play( idSoundA );
     ins.volume = easeInOutQuad( xfade, 1.0, -1.0, 1.0 );
+    ins.setPan( pan );
   }
   if( xfade > 0.0 ) {
     var ins = createjs.Sound.play( idSoundB ); 
+    ins.setPan( pan );
     ins.volume = easeInOutQuad( xfade, 0.0, 1.0, 1.0 );
   }
-}
+};
 
 var ws;
-var lastDate = Date.now()
+var lastDate = Date.now();
 var vals = [];
 var fps = 0;
 var pts = [];
@@ -268,19 +274,19 @@ $(function(){
     }
 
     if( dummyMode ) {
-      vals = [0,0];
-    } 
+      vals = [0,0,0,0];
+    }
     numLines = vals.length;
     
     var lineHeight = stageHeight / (numLines+2);
 
     while( listeners.length < numLines ) {
-      var dummyBPM = 0;
-      if( dummyMode ) {
-        dummyBPM = (Math.random() * 40) + 60;
-      }
-      var listener = new PulseListener( 3.0, dummyBPM );
+      var listener = new PulseListener( 3.0, 0 );
       listeners.push( listener );
+
+      var controlName = "sldPan_"+ listeners.length;
+      $("#controls").append( "<div id='"+controlName+"' class='slider pan'></div>" );
+      $("#"+controlName).slider( {value: 50} );
     }
 
     var thisPoints = [];
@@ -292,12 +298,16 @@ $(function(){
 
       beat = listener.shouldDisplayBeat( thisTime );
       if(beat) {
-          console.log( "beat!" );
+          // console.log( "beat!" );
           playSoundForListenerIndex( i );
       }
 
       thisY = baseline - (listener.normalisedValue()*lineHeight*0.8);
       thisPoints.push( [ traceX, thisY ] );
+
+      var control = $("#sldPan_"+(i+1));
+      //console.log( control );
+      control.css( {top:thisY} );
     };
 
     if( pts.length > 0 ) {
@@ -317,6 +327,40 @@ $(function(){
 
      $("#fps").text( fps + "fps" );
   };
+
+  if( dummyMode ){
+    $(window).keypress(function(e){
+      var code = e.keyCode;
+      // console.log( code );
+      if( (code>48) && (code<53) ){
+        // for keys 1-4, set corresponding listener to a random bpm
+        var listenerIndex = code - 49;
+        console.log( listenerIndex );
+        console.log( listeners );
+        if( listenerIndex < listeners.length ) {
+          var listener = listeners[ listenerIndex ];
+          if( listener.bpm > 0 ) {
+            listener.setBpm( 0 );
+          } else {
+            listener.setBpm( 50.0 + (Math.random()*50.0) );
+          }
+        }
+      } else if( code == 48 ) {
+        // key 0, mute all pulses
+        for (var i = listeners.length - 1; i >= 0; i--) {
+          var listener = listeners[i];
+          listener.setBpm( 0 );
+        }
+      }
+      else if( code == 57 ) {
+        // key 9, set all to new bpm
+        for (var i = listeners.length - 1; i >= 0; i--) {
+          var listener = listeners[i];
+          listener.setBpm( 50.0 + (Math.random()*50.0) );
+        }
+      }
+    });
+  }
 
   createjs.Ticker.addEventListener( "tick", handleTick );
   /*
